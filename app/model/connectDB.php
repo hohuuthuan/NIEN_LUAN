cc8b6950553ea38a463ab822e293fc8484fd108e
hohuuthuan <hohuuthuan789vn@gmail.com>
2024-10-16 09:02:38 +0700

    create project

---
+++ app/model/connectDB.php
@@ -0,0 +1,19 @@
+<?php
+function connectDB() {
+    $host = 'localhost';
+    $dbname = 'phone-shop';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        return null;
+    }
+    return $conn;
+}
+
+?>
